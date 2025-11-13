293740570295f5cd9a92049c68e57737599f9d20
carelee <care.lee@provisionusa.com>
2025-11-13 10:27:34 +0800

    Chango for the response

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,35 @@
 use futures_util::TryFutureExt;
 use http_body_util::Full;
 use hyper::service::service_fn;
-use hyper::{body::{Bytes, Incoming}, server::conn::http1, HeaderMap, Request, Response};
+use hyper::{
+    body::{Bytes, Incoming},
+    server::conn::http1,
+    HeaderMap, Request, Response,
+};
 use hyper_util::rt::TokioIo;
 use std::fs::File;
+use std::net::SocketAddr;
 use std::path::Path;
-use std::{net::SocketAddr};
-use serde_json::{json, Value};
 use tokio::net::TcpListener;
 mod mode;
 use mode::*;
 
-
 async fn hello(req: Request<Incoming>) -> Result<Response<Full<Bytes>>, String> {
     let file = File::open(Path::new("route_response.json")).map_err(|e| e.to_string())?;
     let json: Vec<RouteResponse> = serde_json::from_reader(file).map_err(|e| e.to_string())?;
-    let res = json.iter().find(|e| {
-        req.uri().path() == e.url && e.method.eq_ignore_ascii_case(req.method().as_str())
-    }).ok_or(format!("no route response found"))?;
+    let res = json.iter()
+        .find(|e| req.uri().path().contains(&e.url) && e.method.eq_ignore_ascii_case(req.method().as_str()))
+        .ok_or(format!("no route response found"))?;
     let mut respones = Response::new(Full::new(serde_json::to_vec(&res.response).unwrap().into()));
     respones.headers_mut().extend(req.headers().clone());
-    if let Some(map) = res.headers.clone(){
-        if !map.is_empty(){
+    if let Some(map) = res.headers.clone() {
+        if !map.is_empty() {
             let headers: HeaderMap = (&map).try_into().expect("valid headers");
             respones.headers_mut().extend(headers);
         }
     }
     Ok(respones.into())
 }
-
 
 #[tokio::main]
 async fn main() -> Result<(), String> {
--- src/mode.rs
+++ src/mode.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::iter::Map;
 use serde::{Deserialize, Serialize};
 use serde_json::{Value};
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
@@ -7,5 +6,5 @@ pub(crate) struct RouteResponse {
     pub(crate) url: String,
     pub(crate) method: String,
     pub(crate) headers: Option<HashMap<String, String>>,
-    pub(crate) response: Value,
+    pub(crate) response: Option<Value>,
 }
